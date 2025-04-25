e19f8882c4f1d940deecef739d10f7939979c834
Kailan <kailanlopes1103@gmail.com>
2025-04-25 12:03:49 -0300

    Intializer project, close #1

---
+++ src/main/java/com/codehub/theater_management/TheaterManagementApplication.java
@@ -0,0 +1,13 @@
+package com.codehub.theater_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehub/theater_management/TheaterManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehub.theater_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
