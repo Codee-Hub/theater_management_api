bf2833fe38e1a69cdfefb7a6192e2166066e8268
Kailan <kailanlopes1103@gmail.com>
2025-04-25 18:40:42 -0300

    feat: client entity creation, close #2

---
+++ src/main/java/com/codehub/theater_management/model/Client.java
@@ -0,0 +1,30 @@
+package com.codehub.theater_management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "client")
+@Data //Annotation do lombok para criar os getters e setters, entre ourtas...
+public class Client {
+
+    @Id
+    @Column(name = "id_client")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name",length = 255,nullable = false)
+    private String nome;
+
+    @Column(name = "email",length = 255,nullable = false)
+    private String email;
+
+    @Column(name = "cpf",nullable = false)
+    private BigInteger cpf;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+
+}
