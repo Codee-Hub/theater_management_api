260037ec725736f62b683f654310fcd0d146a813
Kailan <kailanlopes1103@gmail.com>
2025-05-07 22:49:00 -0300

    DTO creation - close #6

--- src/main/java/com/codehub/theater_management/controller/dto/RoomDTO.java
+++ src/main/java/com/codehub/theater_management/controller/dto/RoomDTO.java
@@ -11,35 +11,5 @@ public class RoomDTO {
     private Long idTheater;
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(Integer roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public Long getIdTheater() {
-        return idTheater;
-    }
-
-    public void setIdTheater(Long idTheater) {
-        this.idTheater = idTheater;
-    }
 }
