5d8f50c675dd2c4c0072d5571d3aaefb565bcc20
Kailan <kailanlopes1103@gmail.com>
2025-05-30 11:17:36 -0300

    delecao do security - closes #17

--- src/main/java/com/codehub/theater_management/controller/mapper/SpectacleMapper.java
+++ src/main/java/com/codehub/theater_management/controller/mapper/SpectacleMapper.java
@@ -1,12 +1,11 @@
 package com.codehub.theater_management.controller.mapper;
 
 
-import com.codehub.theater_management.controller.dto.RoomDTO;
 import com.codehub.theater_management.controller.dto.SpectacleDTO;
 import com.codehub.theater_management.model.Room;
 import com.codehub.theater_management.model.Spectacle;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+
 
 @Mapper(componentModel = "spring")
 public interface SpectacleMapper {
